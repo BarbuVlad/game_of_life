06da87f92b1ea16c1acc29ad722d9b6218f94b59
BarbuVlad <barb.vlad@gmail.com>
2021-11-16 16:58:40 +0200

    with_wall

--- sketch.js
+++ sketch.js
@@ -1,10 +1,7 @@
-let i = 0;
-let j = 0;
-
 const rows = 30;
 const cols = 30;
 const resFactor = 20;
-const neighbourhood = 1; ///<go one cell far 
+const neighbourhood = 1; ///<go one cell far TODO
 /*
 Having a canvas of 600x600 = rows^2
 results in a grid of 30 by 30 squares
@@ -16,12 +13,23 @@ let grid = Array(rows).fill()
 .map(entry => Array(cols).fill(0));
 function setup() {
   createCanvas(600, 600);
+  frameRate(5);
+  // grid[10][10]=1;
+  // grid[11][10]=1;
+  // grid[12][10]=1;
+  // grid[15][10]=1;
+  // grid[15][11]=1;
+  // grid[15][12]=1;
+  //grid[12][11]=1;
+  
   grid[10][10]=1;
-  grid[13][13]=1;
-  grid[12][11]=1;
-
- // getNeighboursAlive(grid,13,13);
-
+  grid[11][10]=1;
+  grid[12][10]=1;
+  
+  grid[11][11]=1;
+  
+  grid[11][8]=1;
+  
 }
 
 
@@ -33,11 +41,11 @@ function draw() {
   for(let i = 0; i<rows; i++){
     for(let j = 0; j<cols; j++){
       if(grid[i][j]==0){
-        fill(200);
+        fill(70);
         stroke(1);
         rect(i*resFactor,j*resFactor,resFactor,resFactor);
       } else{
-        fill(50);
+        fill(250);
         stroke(1);
         rect(i*resFactor,j*resFactor,resFactor,resFactor);
       }
@@ -55,35 +63,22 @@ function draw() {
       for(let j = 0; j<cols; j++){
         //for this cell get alive neighbours 
         let count = getNeighboursAlive(grid, i, j);
-        if(grid[i][j] == 1){
-          if(count<=2 || count>3){
-            next_grid[i][j] == 0;
-          }
+        if(grid[i][j] == 0 && count == 3){
+          next_grid[i][j] = 1; //Become alive
+        } else if(grid[i][j] == 1 && count < 2){
+          next_grid[i][j] = 0; //Die, underpopulation
+        }else if(grid[i][j] == 1 && count > 3){
+          next_grid[i][j] = 0; //Die, overpopulation
         } else{
-          if(count==3){
-            next_grid[i][j] == 1;
+          next_grid[i][j] = grid[i][j]; //stay the same
         }
-      
-      }//is 0 
-        //put new cell state in next_grid as same i j 
-      }
-    }
+        
+      }//for
+    }//for
 
 
   //Step3: make curr grid = next grid and redraw
-  gird = next_grid;
-
-
-
-// if(j==600){
-//   j=0;
-//   if(i==600){i=0;}
-// }
-// fill(200, 200, 150);
-// noStroke();
-// rect(i,j,20,20);
-// i+=2;
-// j+=2;
+  grid = next_grid;
 
 }
 
@@ -94,13 +89,12 @@ function getNeighboursAlive(grid, x, y){
       -1-1 |  0-1 | 1-1
       -1 0 |  i j | 1 0
       -1 1 |  0 1 | 1 1 
-
   */
   let count = 0;
   for(let i = -1; i<2; i++){
     for(let j = -1; j<2; j++){
       if(x+i==-1 || x+i==cols || y+j==-1 || y+j==rows){
-        count+=0;
+        count-=1;
       }else{
         count += grid[x+i][y+j];///< state is either 0 or 1
       }
